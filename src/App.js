9fa25b3f0833d1b3eab489fec529ebfa6e9e40e8
Rajeet <rajeetdel@gmail.com>
2019-10-15 12:07:12 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+
+  state = {
+    value: '',
+    emails: []
+  }
+  
+  handleChange = (evt) => {
+    this.setState({
+      value: evt.target.value
+    });
+  };
+
+  handleAdd = (evt) => {
+      evt.preventDefault();
+        
+      var email = this.state.value.trim();
+        
+      if (email) {
+        this.setState({
+          emails: [...this.state.emails, email],
+          value: ''
+        });
+      }
+  };
+
+  handleDelete = (toBeRemoved) => {
+    this.setState({
+      emails: this.state.emails.filter(email => email !== toBeRemoved)
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+      {this.state.emails.map(email => (
+      <div key={email}>
+        {email}
+    
+        <button
+          type="button"
+          onClick={() =>  this.handleDelete(email)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </button>
+      </div>
+      ))}
+
+      <input
+        placeholder="Input the mail address"
+        value={this.state.value}
+        onChange={this.handleChange}
+        onClick={this.handleAdd}
+      />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
